284180d65a73566a48be3daf495f0e0ba47173ec
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2022-01-10 16:02:15 -0800

    home view first draft

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from 'react';
+import { getAllChars } from '../../services/services';
 import CharList from '../../components/CharList';
+
 export default function Home() {
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    getAllChars().then(() => {
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <>
-      <CharList />
+      Avatar Characters
+      {loading ? 'Loading...' : <CharList characters={characters} />}
     </>
   );
 }
